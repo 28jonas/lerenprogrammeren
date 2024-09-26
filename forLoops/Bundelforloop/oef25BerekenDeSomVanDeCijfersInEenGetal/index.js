7a680a9575ba9d07a7d5f26ea1bff35d1171a0e5
28jonas <jonasvandecasteele2002@gmail.com>
2024-09-26 12:25:04 +0200

    Oefening 25

--- forLoops/Bundelforloop/oef25BerekenDeSomVanDeCijfersInEenGetal/index.js
+++ forLoops/Bundelforloop/oef25BerekenDeSomVanDeCijfersInEenGetal/index.js
@@ -1,10 +1,18 @@
 // Geef een getal bv 1234
 let getal = parseInt(prompt("Geef een getal in:"));
+let ingevoerdGetal = getal;
 let somGetallen = 0;
-// kijk hoeveel getallen er zijn met deelbaarheid door 1, 10, 100, 1000,...
-for(let i = 1; i <= getal; i *=10){
-    if(getal % i){
-        somGetallen = somGetallen + (getal%i) ;
-    }
+
+// kijk hoeveel getallen er zijn en delen door 1, 10, 100, 1000,...
+for(var i = 1; i <= getal; i *=10){
+    somGetallen = somGetallen + ((getal % i) / (i / 10));
+    getal = getal - (getal % i);
+    console.log(somGetallen)
 }
-console.log(`De som van ${getal} is ${somGetallen} `)
+
+//laatste getal laten delen, for loop wil de laatste niet meer doen (1234>1000)
+somGetallen = somGetallen + ((getal%i)/(i/10));
+getal = getal - (getal%i);
+
+//uitvoer
+console.log(`De som van ${ingevoerdGetal} is ${somGetallen} `)
